63e59e2b4562962d36b608145e2d6f0f0058994a
Ramil <99542745+Fullerite@users.noreply.github.com>
2023-03-06 13:31:38 +0300

    Removed redundant line of code

--- OCR/flag.py
+++ OCR/flag.py
@@ -2,7 +2,6 @@ import subprocess
 import os
 
 for filename in os.listdir(f"{os.getcwd()}/task"):
-    if filename != "flag.py":
-        text = subprocess.run(["gocr", f"{os.getcwd()}/task/{filename}"], stdout=subprocess.PIPE).stdout  # works only for Linux machines
-        if "{" in str(text):
-            print(filename, text.decode("utf-8"))  # GOCR recognized "0" as "O". After checking it manually, I entered the correct key "Skill{1_ju87_w4n7_70_s4y_u_f0und_17}
+    text = subprocess.run(["gocr", f"{os.getcwd()}/task/{filename}"], stdout=subprocess.PIPE).stdout  # works only for Linux machines
+    if "{" in str(text):
+        print(filename, text.decode("utf-8"))  # GOCR recognized "0" as "O". After checking it manually, I entered the correct key "Skill{1_ju87_w4n7_70_s4y_u_f0und_17}
